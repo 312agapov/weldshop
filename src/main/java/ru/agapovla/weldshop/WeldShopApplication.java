aa3be99c16a4eaec11016c4e5cb81800b9ba3a28
agapovla <312agapov@gmail.com>
2025-06-04 13:16:02 +0400

    first commit

---
+++ src/main/java/ru/agapovla/weldshop/WeldShopApplication.java
@@ -0,0 +1,13 @@
+package ru.agapovla.weldshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeldShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeldShopApplication.class, args);
+	}
+
+}
